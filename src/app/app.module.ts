918c758fa137a1ac807f6fbc8468fb1abdd0e5ae
Simon Bison <simonpeterbison@gmail.com>
2019-03-13 18:02:31 -0500

    added routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+
+const appRoutes :Routes = [
+  {
+    path:'notes',
+    component:NotesComponent
+  },
+  {
+    path:'',
+    component:NotesComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
